bd8bfe24875bcc1e177612372bffa68b3a46206c
BrokenSunny <763184085@qq.com>
2026-08-06 06:57:20 +0800

    refactor! : all

--- lua/repeat/init.lua
+++ lua/repeat/init.lua
@@ -8,17 +8,51 @@ local repeat_operation
 local repeat_operation_count
 local repeat_motion
 local repeat_motion_count
+local repeat_register = { "", "" }
 
 local repeat_tick = -1
+
+local function is_non_zero_number(count)
+  return count and (type(count) == "number") and count ~= 0
+end
+
+local function is_number(count)
+  return count and (type(count) == "number")
+end
 
 local function update_repeat_tick()
   repeat_tick = vim.b.changedtick
 end
 
+local function is_default_register(register)
+  local clipboard = vim.opt.clipboard:get()
+  if vim.tbl_contains(clipboard, "unnamedplus") then
+    return register == "+"
+  end
+  if vim.tbl_contains(clipboard, "unnamed") then
+    return register == "*"
+  end
+  return register == '"'
+end
+
+local function get_register_sequence()
+  local reg = ""
+  if (repeat_register[1] == repeat_operation) and repeat_register[2] ~= "" then
+    local reg_name = is_default_register(vim.v.register) and repeat_register[2] or vim.v.register
+    if reg_name == "=" then
+      reg = '"=' .. vim.fn.getreg("=", 1) .. "\\<CR>"
+    else
+      reg = '"' .. reg_name
+    end
+  end
+  return reg
+end
+
 --- @param count? integer
 function M.operation(count)
   if repeat_operation and repeat_tick == vim.b.changedtick then
-    count = (count and count ~= 0) and count or repeat_operation_count
+    count = is_non_zero_number(count) and count or repeat_operation_count
+    local reg = get_register_sequence()
     local seq = ""
     if type(repeat_operation) == "function" then
       seq = plug_repeat_operation
@@ -26,7 +60,7 @@ function M.operation(count)
       seq = repeat_operation --[[@as string]]
     end
     vim.api.nvim_feedkeys(seq, "i", true)
-    vim.api.nvim_feedkeys(count == 0 and "" or tostring(count), "ni", false)
+    vim.api.nvim_feedkeys(reg .. (count == 0 and "" or tostring(count)), "ni", false)
   else
     vim.api.nvim_feedkeys((count == 0 and "" or count) .. ".", "ni", false)
   end
@@ -34,7 +68,7 @@ end
 
 --- @param count? integer
 function M.motion(count)
-  count = (count and count ~= 0) and count or repeat_motion_count
+  count = is_non_zero_number(count) and count or repeat_motion_count
   --- @type string
   local seq = ""
   if type(repeat_motion) == "function" then
@@ -46,10 +80,20 @@ function M.motion(count)
   vim.api.nvim_feedkeys(count == 0 and "" or tostring(count), "ni", false)
 end
 
+--- @param command string
+--- @param count? integer
+function M.wrap(command, count)
+  local cnt = is_non_zero_number(count) and tostring(count) or ""
+  local preserve = (repeat_tick == vim.b.changedtick) and "<Plug>__repeat_wrap__" or ""
+  local foldopen = vim.o.foldopen
+  local fo = (foldopen == "undo" or foldopen == "all") and "zv" or ""
+  return cnt .. command .. preserve .. fo
+end
+
 --- @param operation string|fun()
 --- @param count? integer
 function M.set_operation(operation, count)
-  repeat_operation_count = count and count or vim.v.count
+  repeat_operation_count = is_number(count) and count or vim.v.count
   repeat_operation = operation
   update_repeat_tick()
 end
@@ -57,15 +101,14 @@ end
 --- @param motion string|fun()
 --- @param count? integer
 function M.set_motion(motion, count)
-  repeat_motion_count = count and count or vim.v.count
+  repeat_motion_count = is_number(count) and count or vim.v.count
   repeat_motion = motion
 end
 
-function M.wrap(command, count)
-  count = (count and count ~= 0) and count or ""
-  local foldopen = vim.o.foldopen
-  local preserve = (repeat_tick == vim.b.changedtick) and "<Plug>__repeat_wrap__" or ""
-  return count .. command .. preserve .. ((foldopen == "undo" or foldopen == "all") and "zv" or "")
+--- @param operation string|fun()
+--- @param register string
+function M.set_register(operation, register)
+  repeat_register = { operation, (register and (type(register) == "string")) and register or "" }
 end
 
 --- @class Repeat.Config.Keymap
